7dd735234e59a0dc2f98ba31fdd59b9db01ec500
bang-star <bang77060986@gmail.com>
2022-05-14 15:29:50 +0900

    프로그래머스 - 연습문제 : 나누어 떨어지는 숫자 배열(문제풀입법 2가지)

---
+++ src/main/java/day2/Exam14.java
@@ -0,0 +1,44 @@
+package day2;
+
+/*
+ *  문제 : 나누어 떨어지는 숫자 배열
+ *  URL : https://programmers.co.kr/learn/courses/30/lessons/12901
+ *  Date : 2022-05-14
+ * */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Exam14 {
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 7, 10};
+        int divisor = 5;
+        solution(arr, 5);
+    }
+
+    public static int[] solution(int[] arr, int divisor) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]%divisor ==0){
+                list.add(arr[i]);
+            }
+        }
+
+        if(list.isEmpty())
+            list.add(-1);
+
+        Collections.sort(list);
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static void solution1(int[] arr, int divisor){
+        int[] answer = Arrays.stream(arr).filter(x -> x % divisor ==0).toArray();
+        if (answer.length == 0) answer = new int[]{-1};
+        Arrays.sort(answer);
+    }
+
+}
