27aa3fb0cb11b72a708339376946d95dc0161c3d
bang-star <bang77060986@gmail.com>
2022-05-15 17:17:20 +0900

    수정 - 2일차 : 부족한금액계산하기
    등차수열의 합으로 풀이하는 방법 추가

--- src/main/java/day2/Exam12.java
+++ src/main/java/day2/Exam12.java
@@ -13,17 +13,23 @@ public class Exam12 {
     // 100원, 200, 300
 
     public static void main(String[] args) {
-        char ch = '\0';
-        System.out.println(solution(3, 20, 4));
+        System.out.println(solution1(3, 20, 4));
     }
 
+    /* 반복문 풀이 방법 */
     public static long solution(int price, int money, int count) {
         long sum = 0;
         for(int i = 0; i<count; i++){
             sum = sum + (price *(i+1));
         }
-        if(money > sum)
-            return 0;
-        return sum - money;
+
+        return money > sum? 0 : sum - money;
+    }
+
+    /* 등차수열의 합 */
+    public static long solution1(int price, int money, int count) {
+        long sum = (count*(2*price + (count-1)*price))/2;
+
+        return money > sum? 0 : sum - money;
     }
 }
