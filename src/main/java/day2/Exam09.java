048101328fbd5c17fdd5ac291e0166ca13e6d8c9
bang-star <bang77060986@gmail.com>
2022-05-15 17:15:36 +0900

    수정 - 2일차 : 핸드폰 번호 구하기
    1. substring을 이용하는 방법으로 변경

--- src/main/java/day2/Exam09.java
+++ src/main/java/day2/Exam09.java
@@ -13,11 +13,14 @@ public class Exam09 {
 
     public static String solution(String phone_number) {
         StringBuilder sb = new StringBuilder();
+
         String front = phone_number.substring(0, phone_number.length()-4);
         String back = phone_number.substring(phone_number.length()-4);
+
         for(int i=0; i<phone_number.length()-4; i++){
             front = front.replace(phone_number.charAt(i), '*');
         }
+
         sb.append(front);
         sb.append(back);
 
