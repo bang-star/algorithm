70b1bd561ca8fe07e6f99300d8a5894e9b374749
bang-star <bang77060986@gmail.com>
2022-05-18 01:10:00 +0900

    생성 - 프로그래멋(연습문제) : 자릿수 더하기
    (요약)
    반복문을 이용하여 자릿수를 나눗셈 연산자를 활용

---
+++ src/main/java/day3/Exam22.java
@@ -0,0 +1,28 @@
+package day3;
+
+/*
+ *  유형 : 연습문제(스택, 재귀함수 등)
+ *  문제 : 자릿수 더하기
+ *  주소 : https://programmers.co.kr/learn/courses/30/lessons/12931
+ *  난이도 : 중하
+ *  point : 1
+ * */
+
+public class Exam22 {
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        System.out.println(solution(99999999));
+        long end = System.nanoTime();
+        System.out.println("수행시간: " + (end - start) + " ns");
+    }
+
+    // 554100
+    public static int solution(int n) {
+        int sum = 0;
+        while (n> 0){
+            sum += n %10;
+            n = n /10;
+        }
+        return sum;
+    }
+}
