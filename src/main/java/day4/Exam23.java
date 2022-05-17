1a63a5fc6a6fd21a2fb7ef1f6444c6def47778f1
bang-star <bang77060986@gmail.com>
2022-05-18 01:15:37 +0900

    생성 - 프로그래멋(연습문제) : 자연수 뒤집어 배열로 만들기
    (요약)
    반복문을 통해서 나눗셈 연산자를 통해 자릿수를 줄이면서 값을 구하는 방법

---
+++ src/main/java/day4/Exam23.java
@@ -0,0 +1,31 @@
+package day4;
+
+/*
+ *  유형 : 연습문제(스택, 재귀함수 등)
+ *  문제 : 자연수 뒤집어 배열로 만들기
+ *  주소 : https://programmers.co.kr/learn/courses/30/lessons/12932
+ *  Date : 2022-05-17
+ *  난이도 : 중하
+ *  point : 3
+ * */
+
+public class Exam23 {
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        System.out.println(solution(12345));
+        long end = System.nanoTime();
+        System.out.println("수행시간: " + (end - start) + " ns");
+    }
+
+    // 554100
+    public static int[] solution(int n) {
+        int[] answer = new int[String.valueOf(n).length()];
+        int idx = 0;
+        while (n > 0){
+            answer[idx] = (int)(n % 10);
+            n /= 10;
+            idx++;
+        }
+        return answer;
+    }
+}
